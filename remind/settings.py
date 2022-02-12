032827a58358826c72aea2d8a908bb2bf075f7ca
Aman Sharma <bobbysharma6593@gmail.com>
2022-02-12 16:28:53 +0530

    settings py changed

--- remind/settings.py
+++ remind/settings.py
@@ -14,10 +14,12 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
-
+STATIC_ROOT = "/home/kudos02/kusedeployment/static"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
